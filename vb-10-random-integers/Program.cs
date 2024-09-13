8a60df5e86563f4569d4c88ceb4f20a691c0f71e
lucasrochasc <lucas_rochadasilvacruz@hotmail.com>
2024-09-13 16:04:52 +0100

    first commit

---
+++ vb-10-random-integers/Program.cs
@@ -0,0 +1,16 @@
+﻿public static class MainClass
+{
+    public static void Main(string[] args)
+    {
+        List<int> original = new List<int>();
+        Random random = new Random(); 
+
+        for (int i = 0; i < 10; i++)
+        {
+            var num = random.Next(1, 51);
+            original.Add(num);
+        }
+
+        Console.WriteLine(string.Format("{0}", string.Join(", ", original)));
+    }
+}
